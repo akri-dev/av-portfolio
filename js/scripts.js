37e66e979a26b6a92165b067a2fbd04cd6000e65
Alec Joseph Burio <alecjoseph.dev@gmail.com>
2023-06-15 18:37:09 +0800

    solved javascript bug - code placement issue

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+// //sample Push an item in a stack
+
+// /* 
+
+// var stackArray = [];
+// var stackTop = 1;
+
+// var pushButton = function () {
+//     var stackItem = document.getElementById("inputBox").value;
+//     console.log(stackTop);
+//     console.log(stackArray[1]);
+//     stackArray[stackTop++] = stackItem; // iterates after encapsulating stackItem value to initial stackTop value to stackArray array
+//     console.log(stackArray[1]);
+
+// }
+
+// document.getElementById("pushButton").onclick = pushButton;
+
+// */
+
+new fullpage('#fullpage', {
+    //options here
+    anchors: ['home', 'resume', 'portfolio', 'connect'],
+    sectionsColor : ['#FFFFFF', '#000000', '#FFBD59', '#FFFFFF']
+});
+
+var resumePage = function () {
+    fullpage_api.moveTo('resume');
+}
+
+var portfolioPage = function () {
+    fullpage_api.moveTo('portfolio');
+}
